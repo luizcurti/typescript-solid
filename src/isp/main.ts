c1803a39fff501a01f02946a991f198f9554e31b
Luiz <luiz_curti@hotmail.com>
2022-10-23 20:50:42 +0100

    SOLID

---
+++ src/isp/main.ts
@@ -0,0 +1,42 @@
+import { ShoppingCart } from './classes/shopping-cart';
+import { Order } from './classes/order';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { NoDiscount } from './classes/discotuns';
+import { EnterpriseCustomer, IndividualCustomer } from './classes/customer';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+// const individualCustomer = new IndividualCustomer(
+//   'Luiz',
+//   'Curti',
+//   '111.111.111-11',
+// );
+
+const enterpriseCustomer = new EnterpriseCustomer(
+  'Emprega grande',
+  '45.5656.46465.00',
+);
+
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  enterpriseCustomer,
+);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lapis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
